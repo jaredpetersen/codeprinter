7a89cb1177a242119132530db44ed23ac465bd1d
Jared Petersen <jaredtoddpetersen@gmail.com>
2018-07-16 00:07:17 -0700

    added public url as basename to router

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className='h-100'>
           <Navbar />
           <Switch>
