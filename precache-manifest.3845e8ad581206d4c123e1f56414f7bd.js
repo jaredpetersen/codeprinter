d073a7e5afbbb2785fcebe4a70c51b764a649fd6
Jared Petersen <jaredtoddpetersen@gmail.com>
2018-12-05 21:18:27 -0800

    Updates

---
+++ precache-manifest.3845e8ad581206d4c123e1f56414f7bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b090461fdac7704122cc",
+    "url": "/codeprinter/static/js/runtime~main.b090461f.js"
+  },
+  {
+    "revision": "5704387553764846edbd",
+    "url": "/codeprinter/static/js/main.57043875.chunk.js"
+  },
+  {
+    "revision": "66f9eb1063e70526093e",
+    "url": "/codeprinter/static/js/1.66f9eb10.chunk.js"
+  },
+  {
+    "revision": "5704387553764846edbd",
+    "url": "/codeprinter/static/css/main.79520d68.chunk.css"
+  },
+  {
+    "revision": "66f9eb1063e70526093e",
+    "url": "/codeprinter/static/css/1.d5eafec8.chunk.css"
+  },
+  {
+    "revision": "f3e09ce94d951fbaf492d261887e060f",
+    "url": "/codeprinter/index.html"
+  }
+];
